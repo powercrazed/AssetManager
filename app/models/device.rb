7a1b6d0b0bb8a173848a406a55fd7ce8f5eb9274
Carl Potter <carl.potter@gmail.com>
2016-12-05 08:47:49 +0000

    add virtual attribute to device model & views to format tag output

--- app/models/device.rb
+++ app/models/device.rb
@@ -2,4 +2,8 @@ class Device < ActiveRecord::Base
   validates :tag, :name, presence: true, uniqueness: true
 
   has_many :licences, :foreign_key => 'device_id', :inverse_of => :device
+
+  def tag_formatted
+    "%04d" % tag
+  end
 end
