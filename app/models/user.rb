d4cedcce9ed9a6d354a5ed3c12f5bccac303ca57
Carl Potter <carl.potter@gmail.com>
2016-12-06 12:41:17 +0000

    add user to device

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   validates :first_name, :last_name, presence: true
 
+  has_one :device, :inverse_of => :user
   has_many :licences, :foreign_key => 'user_id', :inverse_of => :user
 
   def full_name
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161127222358) do
+ActiveRecord::Schema.define(version: 20161205132933) do
 
   create_table "devices", force: :cascade do |t|
     t.string   "name"
@@ -25,7 +25,10 @@ ActiveRecord::Schema.define(version: 20161127222358) do
     t.string   "warranty_type"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "user_id"
   end
+
+  add_index "devices", ["user_id"], name: "index_devices_on_user_id"
 
   create_table "licences", force: :cascade do |t|
     t.string   "vendor"
