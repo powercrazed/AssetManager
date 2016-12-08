829bd888f5a0d76ea3ee8dad5674cc7c04403a03
Carl Potter <carl.potter@gmail.com>
2016-12-08 14:06:54 +0000

    rename users to people - migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161206214631) do
+ActiveRecord::Schema.define(version: 20161208135649) do
 
   create_table "devices", force: :cascade do |t|
     t.string   "name"
@@ -25,10 +25,10 @@ ActiveRecord::Schema.define(version: 20161206214631) do
     t.string   "warranty_type"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.integer  "user_id"
+    t.integer  "person_id"
   end
 
-  add_index "devices", ["user_id"], name: "index_devices_on_user_id"
+  add_index "devices", ["person_id"], name: "index_devices_on_person_id"
 
   create_table "licences", force: :cascade do |t|
     t.string   "vendor"
@@ -41,14 +41,14 @@ ActiveRecord::Schema.define(version: 20161206214631) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.integer  "device_id"
-    t.integer  "user_id"
     t.boolean  "in_use"
+    t.integer  "person_id"
   end
 
   add_index "licences", ["device_id"], name: "index_licences_on_device_id"
-  add_index "licences", ["user_id"], name: "index_licences_on_user_id"
+  add_index "licences", ["person_id"], name: "index_licences_on_person_id"
 
-  create_table "users", force: :cascade do |t|
+  create_table "people", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.string   "location"
