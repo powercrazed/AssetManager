17788723bfd220a17acf62cbaf72b2ea4364dd92
Carl Potter <carl.potter@gmail.com>
2016-12-08 14:07:51 +0000

    rename users to people - models

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,7 +1,7 @@
 class Device < ActiveRecord::Base
   validates :tag, :name, presence: true, uniqueness: true
 
-  belongs_to :user, :inverse_of => :device
+  belongs_to :person, :inverse_of => :device
   has_many :licences, :foreign_key => 'device_id', :inverse_of => :device
 
   def tag_formatted
