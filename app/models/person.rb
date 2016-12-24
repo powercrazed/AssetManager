0c4517449889501b472e6ead40a726ca0c500000
Carl Potter <carl.potter@gmail.com>
2016-12-24 14:36:34 +0000

    fix device -> person relationship in person model

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,7 @@
 class Person < ActiveRecord::Base
   validates :first_name, :last_name, presence: true
 
-  has_one :device, :inverse_of => :Person
+  has_one :device, :inverse_of => :person
   has_many :licences, :foreign_key => 'person_id', :inverse_of => :person
 
   def full_name
