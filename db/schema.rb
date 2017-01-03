101372bb39475d3eb5c6079c213b834d2d749007
Carl Potter <carl.potter@gmail.com>
2017-01-03 09:23:53 +0000

    add notes to people

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170103092143) do
+ActiveRecord::Schema.define(version: 20170103092239) do
 
   create_table "devices", force: :cascade do |t|
     t.string   "name"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20170103092143) do
     t.string   "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "notes"
   end
 
   create_table "users", force: :cascade do |t|
