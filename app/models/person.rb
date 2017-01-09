ded424ecd534981b51792aa0c32bf0256bc0efbb
Carl Potter <carl.potter@gmail.com>
2017-01-09 09:23:23 +0000

    add import to person model

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,5 @@
 class Person < ActiveRecord::Base
+  require 'csv'
   validates :first_name, :last_name, presence: true
 
   has_one :device, :inverse_of => :person
@@ -6,5 +7,18 @@ class Person < ActiveRecord::Base
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+
+      person_hash = row.to_hash
+      person = Person.where(first_name: person_hash["first_name"], last_name: person_hash["last_name"])
+
+      # create if not in database
+      if person.count == 0
+        Person.create! person_hash
+      end
+    end
   end
 end
