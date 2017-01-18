2f3c38261a2b9de179958bd563836266139d1eae
Carl Potter <carl.potter@gmail.com>
2017-01-18 18:34:20 +0000

    add user lookup to device import

--- app/models/device.rb
+++ app/models/device.rb
@@ -10,12 +10,15 @@ class Device < ActiveRecord::Base
 
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
-
       device_hash = row.to_hash
       device = Device.where(name: device_hash["name"])
-
       # create if not in database
       if device.count == 0
+        person = Person.where(first_name: device_hash["first_name"], last_name: device_hash["last_name"])
+        # throw away person name columns after db lookup
+        ["first_name", "last_name"].each { |k| device_hash.delete(k) }
+        # add person_id to device hash, id gets set to nil if not found in db
+        device_hash[:person_id] = person.first.try(:id)
         Device.create! device_hash
       end
     end
