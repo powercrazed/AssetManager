e9a95335dc1c0a9c8161470cb49d42bd95ce338e
Carl Potter <carl.potter@gmail.com>
2017-01-19 08:59:57 +0000

    add user & device lookup to licence import

--- app/models/licence.rb
+++ app/models/licence.rb
@@ -12,6 +12,13 @@ class Licence < ActiveRecord::Base
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
       licence_hash = row.to_hash
+      device = Device.where(name: licence_hash["device_name"])
+      person = Person.where(first_name: licence_hash["first_name"], last_name: licence_hash["last_name"])
+      # throw away device & person name columns after db lookups
+      ["device_name", "first_name", "last_name"].each { |k| licence_hash.delete(k) }
+      # add id's to licence hash, id's get set to nil if not found in db
+      licence_hash[:device_id] = device.first.try(:id)
+      licence_hash[:person_id] = person.first.try(:id)
       Licence.create! licence_hash
     end
   end
