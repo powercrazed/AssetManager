0972f6ae2b9ad147fa97025969d09f42eacaa468
Carl Potter <carl.potter@gmail.com>
2017-01-21 23:08:41 +0000

    add devise test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,4 +7,7 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  class ActionController::TestCase
+    include Devise::Test::ControllerHelpers
+  end
 end
