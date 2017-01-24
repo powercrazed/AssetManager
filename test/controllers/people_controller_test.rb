db7b144102f0cb6f96a29b7cb4681533f2315b87
Carl Potter <carl.potter@gmail.com>
2017-01-24 09:08:16 +0000

    fix people controller tests by adding user signin

--- test/controllers/people_controller_test.rb
+++ test/controllers/people_controller_test.rb
@@ -12,11 +12,13 @@ class PeopleControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
+    sign_in users(:one)
     get :new
     assert_response :success
   end
 
   test "should create person" do
+    sign_in users(:one)
     assert_difference('Person.count') do
       post :create, person: { first_name: @person.first_name, last_name: @person.last_name, location: @person.location }
     end
@@ -30,16 +32,19 @@ class PeopleControllerTest < ActionController::TestCase
   end
 
   test "should get edit" do
+    sign_in users(:one)
     get :edit, id: @person
     assert_response :success
   end
 
   test "should update person" do
+    sign_in users(:one)
     patch :update, id: @person, person: { first_name: @person.first_name, last_name: @person.last_name, location: @person.location }
     assert_redirected_to person_path(assigns(:person))
   end
 
   test "should destroy person" do
+    sign_in users(:one)
     assert_difference('Person.count', -1) do
       delete :destroy, id: @person
     end
